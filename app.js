74500584fca7d748d567b6d028fd44950cc51a17
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-05-29 21:42:44 -0400

    create add command

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const chalk = require('chalk');
 const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
+//Customize yargs version
+
+yargs.version('1.1.0');
+
+//Create add command
+yargs.command({
+  command: 'Add',
+  describe: 'Add a new note ',
+  handler: function() {
+    console.log('Adding a new note!')
+  }
+});
+
+
+
 console.log(yargs.argv);
 
 
