e4d6f744373c80d18eca1ac045d7312c7d578eb9
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-10 15:54:09 -0400

    parsing jason data

---
+++ playground/1-json.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+/***
+ * Learning about Jason Objects
+ */
+
+// const book = {
+//   title: 'Ego is the enemy',
+//   author: 'Ryan Holiday'
+// }
+
+// const bookJSON = JSON.stringify(book);
+
+// // To create JSON file
+// fs.writeFileSync('1-json.json',bookJSON);
+const dataBuffer = fs.readFileSync('1-json.json');
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+console.log(data.title);
