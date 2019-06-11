2aafacdef93547094675ff52d24c1a7befe395e3
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-11 15:57:20 -0400

    removing notes function

--- notes.js
+++ notes.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const getNotes = function () {
-  return 'Your notes... '
-}
+const getNotes = function() {
+  return "Your notes... ";
+};
 
 const addNote = function(title, body) {
   const notes = loadNotes();
@@ -16,42 +16,37 @@ const addNote = function(title, body) {
       body: body
     });
     saveNotes(notes);
-    console.log('New note added! ');
+    console.log("New note added! ");
   } else {
-    console.log('Note title taken! ');
+    console.log("Note title taken! ");
   }
 };
 
-const saveNotes = function (notes) {
-  const dataJSON = JSON.stringify(notes);
-  fs.writeFileSync('notes.json', dataJSON);
-  
+const removeNotes = function(title) {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter(function(note) {
+    return note.title !== title;
+  });
+  saveNotes(notesToKeep);
 };
 
+const saveNotes = function(notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
 
-const loadNotes = function () {
+const loadNotes = function() {
   try {
-      const dataBuffer = fs.readFileSync("notes.json");
-      const dataJSON = dataBuffer.toString();
-      return JSON.parse(dataJSON);
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
   } catch (error) {
     return [];
   }
-
-  
-}
-
-const removeNotes = function(title) {
-  // const notes = loadNotes();
-  // const duplicateNotes = notes.filter(function(note) {
-  //   return note.title === title;
-  console.log(title);
-  };
-
+};
 
 module.exports = {
   getNotes: getNotes,
   addNote: addNote,
-  removeNotes:removeNotes
+  removeNotes: removeNotes
 };
-
