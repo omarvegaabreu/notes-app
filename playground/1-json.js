545f2dd8bbe6dccfdbf2e94837cb15b90dac9f93
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-10 20:47:51 -0400

    bug in code

--- playground/1-json.js
+++ playground/1-json.js
@@ -18,10 +18,11 @@ const fs = require('fs');
 // console.log(data.title);
 const personDataBuffer =fs.readFileSync('1-json.json');
 const personData = personDataBuffer.toString();
-const personDataJSON = JSON.parse(personData);
 
 personData.name= 'Jose';
 personData.age = 54;
+
+const personDataJSON = JSON.parse(personData);
 
 const userJASON = JSON.stringify(personDataJSON);
 fs.writeFileSync('1-json.json', userJASON);
