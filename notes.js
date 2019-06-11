908cb49bc9d8496cb11f3557538b35cc56136c66
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-11 16:07:25 -0400

    color coded messages working

--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const chalk = require("chalk");
 
 const getNotes = function() {
   return "Your notes... ";
@@ -27,7 +28,12 @@ const removeNotes = function(title) {
   const notesToKeep = notes.filter(function(note) {
     return note.title !== title;
   });
-  saveNotes(notesToKeep);
+  if (notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse("Note removed!"));
+    saveNotes(notesToKeep);
+  } else {
+    console.log(chalk.red.inverse("No note found!!!"));
+  }
 };
 
 const saveNotes = function(notes) {
