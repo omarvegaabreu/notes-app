35b35fb8157bcd1570daee64af7c2236a0e12352
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-10 21:55:47 -0400

    try catch method

--- notes.js
+++ notes.js
@@ -1,13 +1,27 @@
+const fs = require('fs');
+
 const getNotes = function () {
   return 'Your notes... '
 }
 
 const addNote = function (title, body) {
   const notes = loadNotes();
+  notes.push({
+    title: title,
+    body: body
+  });
+  console.log(notes);
 };
 
 const loadNotes = function () {
-  const dataBuffer = fs.readFileSync('notes.json');
+  try {
+      const dataBuffer = fs.readFileSync("notes.json");
+      const dataJSON = dataBuffer.toString();
+      return JSON.parse(dataJSON);
+  } catch (error) {
+    return [];
+  }
+
   
 }
 
