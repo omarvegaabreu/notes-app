cd8d83b16873291ae37ab89ca40a2bf4387d44b8
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-13 14:24:01 -0400

    arrow function2

--- playground/name.js
+++ playground/name.js
@@ -1,17 +1,12 @@
 /**
  * arrow functions explained
  */
-
 // const square = function(x) {
 //   return x * x;
 // };
-
 // console.log(square(3));
-
 // const square = x => {
 //   return x * x;
 // };
-
 // console.log(x)
-// const square = (x) => x * x;
-// console.log(square(2));
+function name(params) {}
