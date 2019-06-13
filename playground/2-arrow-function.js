5b7ae6830c379a20aa31613887b9d5e7d4f5492f
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-13 14:20:24 -0400

    arrow functions

---
+++ playground/2-arrow-function.js
@@ -0,0 +1,15 @@
+/**
+ * arrow functions explained
+ */
+
+// const square = function(x) {
+//   return x * x;
+// };
+
+// console.log(square(3));
+
+// const square = x => {
+//   return x * x;
+// };
+
+// console.log(x)
