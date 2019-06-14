7d818018da4945ba0f0f450f9b7ca805da2699f5
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-14 15:16:14 -0400

    arrow function continued

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -15,3 +15,12 @@
 // console.log(x)
 // const square = (x) => x * x;
 // console.log(square(2));
+
+const event = {
+  name: "Birthday Party ",
+  printGuestList() {
+    console.log("Guest list for " + this.name);
+  }
+};
+
+event.printGuestList();
