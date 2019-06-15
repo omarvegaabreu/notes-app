4d998c65b5349f2fca28e13d6e27fdd89b7a0019
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-14 21:54:44 -0400

    arrow function

---
+++ playground/3-arrow-challenge.js
@@ -0,0 +1,43 @@
+//
+// Goal: Create method to get incomplete tasks
+//
+// 1. Define getTasksToDo method
+// 2. Use filter to to return just the incompleted tasks (arrow function)
+// 3. Test your work by running the script
+
+// const event = {
+//   name: " Birthday Party",
+//   guestList: ["Andrew ", "Jen ", "Mike "],
+//   printGuestList() {
+//     console.log("Guest list for " + this.name);
+//     this.guestList.forEach(guest => {
+//       console.log(guest + "is attending" + this.name);
+//     });
+//   }
+// };
+// event.printGuestList();
+
+const tasks = {
+  tasks: [
+    {
+      text: "Grocery shopping",
+      completed: true
+    },
+    {
+      text: "Clean yard",
+      completed: false
+    },
+    {
+      text: "Film course",
+      completed: false
+    }
+  ],
+  getTasksToDo() {
+    console.log("Guest list for " + this.tasks);
+    this.tasks.forEach(task => {
+      console.log(task + "is attending" + this.tasks);
+    });
+  }
+};
+
+console.log(tasks.getTasksToDo());
