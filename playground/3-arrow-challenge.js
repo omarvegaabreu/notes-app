b88f732c290a80e5202b4ed5925dbeeaf0a88779
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-16 18:25:42 -0400

    added comments to the code

--- playground/3-arrow-challenge.js
+++ playground/3-arrow-challenge.js
@@ -1,9 +1,4 @@
-//
-// Goal: Create method to get incomplete tasks
-//
-// 1. Define getTasksToDo method
-// 2. Use filter to to return just the incompleted tasks (arrow function)
-// 3. Test your work by running the script
+//code added for testing functions in the application.
 
 const tasks = {
   tasks: [
